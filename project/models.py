3180be215b1865483b1e6d0b80538073fbecd236
leeroymk <nikekursk@gmail.com>
2023-03-13 07:32:15 +0300

    microblog

--- project/models.py
+++ project/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from project import db
+from hashlib import md5
+from project import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
-from project import login
 from flask_login import UserMixin
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -20,6 +23,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,8 +37,8 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-# Если не работает - нужно заменить return на db.session.get(User, int(id))
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
